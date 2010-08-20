253e84920683a7eccdfe6d58597d84cb9ee6694a
tgalvin <ext-tom.galvin@nokia.com>
2010-08-20 17:40:33 +0100

    Separate out ExpressionId into separate module

--- RuoteAMQP/flow_expression_id.py
+++ RuoteAMQP/flow_expression_id.py
@@ -1,55 +1,7 @@
-import re
+from expression_id import ExpressionId
 
 class FeiException(Exception):
     pass
-
-class ExpressionId(object):
-    """
-    The ExpressionId
-    FIXME more documentation
-    """
-
-    CHILD_SEP = "_"
-
-    def __init__(self, *args):
-        self.args = args
-
-    def __iter__(self):
-        """
-        @ytype: C{int}
-        @yield: The arguments that make up the ExpressionId 
-        """
-        for arg in self.args: yield int(arg)
-
-    def __str__(self):
-        """
-        @rtype: C{string}
-        @return:form '^\d{0,2}%s\d{0,2}%s\d{0,2}$'
-        """
-        return self.CHILD_SEP.join([str(arg) for arg in self])
-
-    @property 
-    def child_id(self):
-        """
-        @rtype: C{int}
-        @return: The child id
-        """
-        return int(self.args[-1]) 
-
-    @property 
-    def parent_id(self):
-        """
-        @rtype: L{ExpressionId}
-        @return: The ExpressionId of the parent
-        """
-        return ExpressionId(*self.args[:-1])
-
-    def __eq__(self, expression_id):
-        """
-        @rtype: C{bool}
-        @return: Are the ExpressionIds equal
-        """
-        return (self.args == expression_id.args)
 
 class FlowExpressionId(object):
     """
--- RuoteAMQP/tests/test_flow_expression_id.py
+++ RuoteAMQP/tests/test_flow_expression_id.py
@@ -1,28 +1,7 @@
 import unittest
 
-from flow_expression_id import ExpressionId, FlowExpressionId, FeiException 
-
-
-class TestExpressionId(unittest.TestCase):
-
-    def test_str(self):
-        self.assertEquals("1_2_3", str(ExpressionId(1,2,3)))
-
-    def test_iter(self):
-        self.assertEquals([1, 2, 3], list(ExpressionId(1,2,3)))
-
-    def test_child_id(self):
-        self.assertEquals(4, ExpressionId(1, 2, 3, 4).child_id)
-
-    def test_parent_id(self):
-        self.assertEquals("1_2_3", str(ExpressionId(1, 2, 3, 4).parent_id))
-
-    def test_eq(self):
-        expid_1 = ExpressionId(1, 2, 3)
-        expid_2 = ExpressionId(1, 2, 3)
-        self.assertEquals(expid_1, expid_2)
-        expid_3 = ExpressionId(1, 2, 3, 4)
-        self.assertFalse(expid_1 == expid_3) 
+from expression_id import ExpressionId
+from flow_expression_id import FlowExpressionId, FeiException 
 
 class TestFlowExpressionId(unittest.TestCase):
 
